9232d240cf3001be987a6cfa9b138a48723bcd4e
Shashank Dabral <a0393223@ti.com>
2017-12-25 12:58:18 -0600

    fix-4

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,8 +15,8 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
-  VectorXd rmse(5);
-  rmse  << 0,0,0,0,0;
+  VectorXd rmse(4);
+  rmse  << 0,0,0,0;
   if(estimations.size() != ground_truth.size()
 	|| estimations.size() == 0){
     cout << "Invalid estimation or ground_truth data" << endl;
